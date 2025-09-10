e97340f04226b98462a30d4c7b188275131b91e7
Jalon Wong <jalonwong@gmail.com>
2025-09-10 15:01:59 -0500

    Add an elapsed function

--- src/lib.rs
+++ src/lib.rs
@@ -70,6 +70,10 @@ pub trait TickInstant: Copy {
     fn now() -> Self;
     /// Returns the amount of ticks elapsed from another instant to this one.
     fn tick_since(self, earlier: Self) -> u32;
+    /// Returns the amount of ticks elapsed since this instant.
+    fn tick_elapsed(self) -> u32 {
+        Self::now().tick_since(self)
+    }
 }
 
 /// Can be implement for `yield`, `sleep` or do nothing.
